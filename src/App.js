ad41bc676934b5df282672045956a53ff356f2c8
uaalumni02 <dbell1911@gmail.com>
2024-02-11 13:52:06 -0600

    fetch profile data by id

--- src/App.js
+++ src/App.js
@@ -2,23 +2,15 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Profile from "./pages/profile";
 
-
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
-      {/* <QueryClientProvider client={queryClient}> */}
-        <BrowserRouter>
-          {/* <UserContextProvider> */}
-            <Routes>
-              
-              <Route exact path="/profile" element={<Profile />} />
-              
-              
-            </Routes>
-          {/* </UserContextProvider> */}
-        </BrowserRouter>
-      {/* </QueryClientProvider> */}
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   MDBCard,
   MDBCardBody,
@@ -8,16 +8,43 @@ import {
 } from "mdb-react-ui-kit";
 
 const Profile = () => {
+  const [name, setName] = useState("");
+  const [bio, setBio] = useState("");
+
+  const fetchProfileData = () => {
+    const url = window.location.pathname;
+    const id = url.substring(url.lastIndexOf("/") + 1);
+    fetch("http://localhost:3000/api/profile/" + id, {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((response) => {
+        console.log(response);
+        setName(response.data.name);
+        setBio(response.data.bio);
+      })
+      .catch((error) => console.error("Error:", error));
+  };
+  useEffect(() => {
+    fetchProfileData();
+  });
+
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <MDBCard style={{ width: '400px', height: '200px' }} >
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <MDBCard style={{ width: "400px", height: "200px" }}>
         <MDBCardBody>
-          <MDBCardTitle>Profile</MDBCardTitle>
+          <MDBCardTitle>{name}</MDBCardTitle>
           <MDBCardText>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+           {bio}
           </MDBCardText>
-          <MDBBtn>Button</MDBBtn>
+          <MDBBtn>Edit</MDBBtn>
         </MDBCardBody>
       </MDBCard>
     </div>
